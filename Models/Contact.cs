fc00fac85d16c68b620345d422012b1646f8e244
Koki Nakeni <41635069+KokiNakeni@users.noreply.github.com>
2022-09-14 14:39:32 +0200

    New commit

---
+++ Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContactsAddressManagement.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+        [MaxLength(100)]
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
+        [MaxLength(20)]
+        public string CellphoneNumber { get; set; }
+        public virtual ContactAddress ContactAddress { get; set; }
+    }
+}
